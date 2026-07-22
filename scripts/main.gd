54baae54ab2759785974557a6452ce7650ad5d13
Harper Stoeckle <harper.stoeckle@gmail.com>
2026-07-22 17:02:42 -0600

    Add some basic beat timing handling code

---
+++ scripts/main.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+const SUBDIVISIONS_PER_BEAT: int = 4
+
+
+@onready var _music_player: AudioStreamPlayer = $MusicPlayer
+@onready var _metronome_player: AudioStreamPlayer = $MetronomePlayer
+
+
+var _next_subdivision_to_handle: int = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var stream := _music_player.stream as AudioStreamOggVorbis
+	if not _music_player.playing or not stream: return
+
+	var subdiv_duration: float = 60.0 / stream.bpm / SUBDIVISIONS_PER_BEAT
+	var cur_subdivision: int = floori(_music_player.get_playback_position() / subdiv_duration)
+
+	for i in range(_next_subdivision_to_handle, cur_subdivision + 1):
+		_handle_subdivision(i)
+		_next_subdivision_to_handle = i + 1
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		_next_subdivision_to_handle = 0
+		_music_player.play()
+
+# Handle logic for the passing of the nth subdivision.
+func _handle_subdivision(n: int) -> void:
+	if n % SUBDIVISIONS_PER_BEAT == 0:
+		_metronome_player.play()
